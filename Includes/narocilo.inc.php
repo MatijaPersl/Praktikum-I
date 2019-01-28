6377455f54e929a969e2c05aa2e7f9027ec383af
Matija <matispersl@gmail.com>
2019-01-28 14:54:50 +0100

    .

--- Includes/narocilo.inc.php
+++ Includes/narocilo.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 if (isset($_POST['narocilo-submit'])) {
+  error_reporting(0);
   require 'dbh.inc.php';
 
   $idTaco = $_POST['narocilo-submit'];
@@ -9,6 +10,9 @@ if (isset($_POST['narocilo-submit'])) {
           SELECT nameTaco, priceTaco FROM taco
           WHERE idTaco = $idTaco";
   $result = mysqli_query($conn, $sql);
+
+  header("Location: ../index.php?order=success");
+
 
 }
 
